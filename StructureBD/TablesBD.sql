67370e511822caf3b6617327c844643523864034
Nacho <nachogamallolafon123@gmail.com>
2025-04-14 21:09:25 +0200

    First commit, with the structure of the bd initiated.

---
+++ StructureBD/TablesBD.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE [TOOL_INTERCHANGE]
+
+GO
+
+USE [TOOL_INTERCHANGE]
+
+GO
+
+CREATE TABLE COMUNITY (
+
+	idComunity INT IDENTITY PRIMARY KEY,
+	name VARCHAR(100)
+
+);
+
+CREATE TABLE CITY (
+
+	idCity INT IDENTITY PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	postalCode CHAR(5) NOT NULL,
+	idComunity INT NOT NULL,
+	province VARCHAR(100),
+	FOREIGN KEY idComunity REFERENCES idComunity(idComunity) 
+
+);
+
+GO
+
+CREATE TABLE DIRECTION (
+
+	idDirection INT IDENTITY PRIMARY KEY,
+	street VARCHAR(150) NOT NULL,
+	postalCode CHAR(5) NOT NULL,
+	Number INT NOT NULL,
+	idCity INT,
+	DoorNumber VARCHAR(5),
+	StairNumber VARCHAR(5),
+	FOREIGN KEY idCity REFERENCES idCity(idCity)
+	
+);
+
+GO
+
+CREATE TABLE USERS (
+	idUser INT IDENTITY PRIMARY KEY,
+	nameUser VARCHAR(40) NOT NULL,
+	firtSurname VARCHAR(40) NOT NULL,
+	lastSurname VARCHAR(40) NOT NULL,
+	dateOfBirth DATE NOT NULL,
+	isSeller BIT NOT NULL,
+	idDirection INT,
+
+
+	FOREIGN KEY idDirection REFERENCES idDirection(idDirection)
+);
